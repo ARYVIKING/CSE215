c7a52601252408b165e57c98bf54f9a39a6fc1e8
Abdur Rahman Yasin <abdurrahmanyasin999@gmail.com>
2026-06-27 15:52:41 +0600

    Create vsgdva.java

---
+++ vsgdva.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class Method {
+
+    static int max(int arr[])
+    {
+        int mx =0;
+        for(int i=0;i<10;i++)
+        {
+            mx = arr[0];
+            if (arr[i]>mx)
+                mx = arr[i];
+
+        }
+        return mx;
+    }
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int arr[]= new int [10];
+        int mx = 0;
+        int sum = 0;
+        for(int i=0;i<10;i++)
+        {
+            arr[i] = scanner.nextInt();
+            sum += arr[i];
+        }
+        for(int i=0;i<10;i++)
+        {
+            mx = arr[0];
+            if (arr[i]>mx)
+                mx = arr[i];
+
+        }
+
+        System.out.print("Sum is : "+sum +"\n");
+        System.out.print("Max is : "+ mx +"\n");
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    LM4PP1*/
+
+/*LM4PP2
+    */
